d5033160cb64f6612647fb47c60f13a0f82b5b69
William Turner <william.turner@aero.bombardier.com>
2015-03-30 11:53:52 -0400

    Personally endorsing the project

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='BIFFY',
       version='1.0.0',
       description='Excel 2007-2010 Binary Workbook (BIFF12) parser',
       author='William Turner',
-      author_email='william.turner@aero.bombardier.com',
+      author_email='willtur.will@gmail.com',
       packages=['biffy'])
