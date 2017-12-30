c7c996fd3d6d6707a6c77bab16e396058beebcb3
William Turner <willtur.will@gmail.com>
2017-12-30 11:32:01 -0500

    Add some RK number test cases

--- test/datareader_test.py
+++ test/datareader_test.py
@@ -31,9 +31,27 @@ class DataReaderTestCase(unittest.TestCase):
     def test_read_double(self):
         pass
 
-    @unittest.skip('TODO')
+    # TODO: Add negative cases
     def test_read_rk(self):
-        pass
+        # RK type 0 IEEE
+        reader = DataReader(b'\x00\x00\xF0\x3F')
+        self.assertEqual(reader.read_rk(), 1)
+        self.assertEqual(reader.read_rk(), None)
+
+        # RK type 1 IEEE x 100
+        reader = DataReader(b'\x01\xC0\x5E\x40')
+        self.assertEqual(reader.read_rk(), 1.23)
+        self.assertEqual(reader.read_rk(), None)
+
+        # RK type 2 integer
+        reader = DataReader(b'\x3A\x85\xF1\x02')
+        self.assertEqual(reader.read_rk(), 12345678)
+        self.assertEqual(reader.read_rk(), None)
+
+        # RK type 3 integer x 100
+        reader = DataReader(b'\x3B\x85\xF1\x02')
+        self.assertEqual(reader.read_rk(), 123456.78)
+        self.assertEqual(reader.read_rk(), None)
 
     def test_read_string(self):
         reader = DataReader(b'\x04\x00\x00\x00T\x00E\x00S\x00T\x00')
