2511890e5d8d1fa3f9f45f77497b80a2fa803e6e
William Turner <william.turner@aero.bombardier.com>
2018-10-31 19:34:13 -0400

    Setting version to 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ proc.wait()
 
 setup(
     name='pyxlsb',
-    version='1.0.4',
+    version='1.1.0',
 
     description='Excel 2007-2010 Binary Workbook (xlsb) parser',
     long_description=README,
