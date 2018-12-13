29a0a6f7f79373c231c7a056296c6b61ac48a9a1
William Turner <william.turner@aero.bombardier.com>
2018-12-13 18:14:57 -0500

    Change readme to rst and change license to MIT
    Resolves #13

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 import os.path
 from setuptools import setup
-from subprocess import Popen, PIPE
 
 # Get a handy base dir
 project_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Use pandoc to convert the Markdown readme to RST for prtty printing on PyPI
-proc = Popen(['pandoc', '--from', 'markdown', '--to', 'rst', '--output', '-', os.path.join(project_dir, 'README.md')], stdout=PIPE)
-README = proc.stdout.read().decode('utf-8')
-proc.stdout.close()
-proc.wait()
+with open(os.path.join(project_dir, 'README.rst')) as f:
+    README = f.read()
 
 setup(
     name='pyxlsb',
     version='1.1.0',
 
-    description='Excel 2007-2010 Binary Workbook (xlsb) parser',
+    description='Excel 2007+ Binary Workbook (xlsb) parser',
     long_description=README,
 
     author='William Turner',
@@ -23,12 +19,12 @@ setup(
 
     url='https://github.com/wwwiiilll/pyxlsb',
 
-    license='LGPLv3+',
+    license='MIT',
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
 
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
