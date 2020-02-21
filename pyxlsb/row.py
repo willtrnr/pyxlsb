f1d61a17959910a70206e5d9cb1f992fba717c00
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-02-22 03:25:57 +0600

    Bug fix missing colums in iter row (#25)

--- pyxlsb/row.py
+++ pyxlsb/row.py
@@ -30,6 +30,6 @@ class Row(object):
     def _add_cell(self, col, *args, **kwargs):
         c = Cell(self, col, *args, **kwargs)
         self._cells[col] = c
-        if col > self._cols:
-            self._cols = col
+        if col >= self._cols:
+            self._cols = col + 1
         return c
