018f55ccbe1fb1b91c4d8c1fc0d77605ef0e89f7
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-02-22 08:30:16 +0600

    Update styles.py

--- pyxlsb/styles.py
+++ pyxlsb/styles.py
@@ -3,44 +3,47 @@ from . import recordtypes as rt
 from .recordreader import RecordReader
 from .records import FormatRecord
 
+
 class Styles(object):
 
-    _default_format = {0: {'format': 'General', 'dtype': ''},
-                                             1: {'format': '0', 'dtype': 'float64'},
-                                             2: {'format': '0.00', 'dtype': 'float64'},
-                                             3: {'format': '#,##0', 'dtype': 'float64'},
-                                             4: {'format': '#,##0.00', 'dtype': 'float64'},
-                                             5: {'format': '($#,##0_);($#,##0)', 'dtype': 'float64'},
-                                             6: {'format': '($#,##0_);[Red]($#,##0)', 'dtype': 'float64'},
-                                             7: {'format': '($#,##0.00_);($#,##0.00)', 'dtype': 'float64'},
-                                             8: {'format': '($#,##0.00_);[Red]($#,##0.00)', 'dtype': 'float64'},
-                                             9: {'format': '0%', 'dtype': 'float64'},
-                                             10: {'format': '0.00%', 'dtype': 'float64'},
-                                             11: {'format': '0.00E+00', 'dtype': 'float64'},
-                                             12: {'format': '# ?/?', 'dtype': 'float64'},
-                                             13: {'format': '# ??/??', 'dtype': 'float64'},
-                                             14: {'format': 'm/d/yy', 'dtype': 'datetime'},
-                                             15: {'format': 'd-mmm-yy', 'dtype': 'datetime'},
-                                             16: {'format': 'd-mmm', 'dtype': 'datetime'},
-                                             17: {'format': 'mmm-yy', 'dtype': 'datetime'},
-                                             18: {'format': 'h:mm AM/PM', 'dtype': 'datetime'},
-                                             19: {'format': 'h:mm:ss AM/PM', 'dtype': 'datetime'},
-                                             20: {'format': 'h:mm', 'dtype': 'datetime'},
-                                             21: {'format': 'h:mm:ss', 'dtype': 'datetime'},
-                                             22: {'format': 'm/d/yy h:mm', 'dtype': 'datetime'},
-                                             37: {'format': '(#,##0_);(#,##0)', 'dtype': 'float64'},
-                                             38: {'format': '(#,##0_);[Red](#,##0)', 'dtype': 'float64'},
-                                             39: {'format': '(#,##0.00_);(#,##0.00)', 'dtype': 'float64'},
-                                             40: {'format': '(#,##0.00_);[Red](#,##0.00)', 'dtype': 'float64'},
-                                             41: {'format': '_(* #,##0_);_(* (#,##0);_(* "-"_);_(_)', 'dtype': 'float64'},
-                                             42: {'format': '_($* #,##0_);_($* (#,##0);_($* "-"_);_(_)', 'dtype': 'float64'},
-                                             43: {'format': '_(* #,##0.00_);_(* (#,##0.00);_(* "-"??_);_(_)', 'dtype': 'float64'},
-                                             44: {'format': '_($* #,##0.00_);_($* (#,##0.00);_($* "-"??_);_(_)', 'dtype': 'float64'},
-                                             45: {'format': 'mm:ss', 'dtype': 'datetime'},
-                                             46: {'format': '[h]:mm:ss', 'dtype': 'datetime'},
-                                             47: {'format': 'mm:ss.0', 'dtype': 'datetime'},
-                                             48: {'format': '##0.0E+0', 'dtype': 'float64'},
-                                             49: {'format': '@', 'dtype': 'string'}}
+    _builtin_formats = {
+        0: FormatRecord(fmtId=0, fmtCode='General'),
+        1: FormatRecord(fmtId=1, fmtCode='0'),
+        2: FormatRecord(fmtId=2, fmtCode='0.00'),
+        3: FormatRecord(fmtId=3, fmtCode='#,##0'),
+        4: FormatRecord(fmtId=4, fmtCode='#,##0.00'),
+        5: FormatRecord(fmtId=5, fmtCode='($#,##0_);($#,##0)'),
+        6: FormatRecord(fmtId=6, fmtCode='($#,##0_);[Red]($#,##0)'),
+        7: FormatRecord(fmtId=7, fmtCode='($#,##0.00_);($#,##0.00)'),
+        8: FormatRecord(fmtId=8, fmtCode='($#,##0.00_);[Red]($#,##0.00)'),
+        9: FormatRecord(fmtId=9, fmtCode='0%'),
+        10: FormatRecord(fmtId=10, fmtCode='0.00%'),
+        11: FormatRecord(fmtId=11, fmtCode='0.00E+00'),
+        12: FormatRecord(fmtId=12, fmtCode='# ?/?'),
+        13: FormatRecord(fmtId=13, fmtCode='# ??/??'),
+        14: FormatRecord(fmtId=14, fmtCode='m/d/yy'),
+        15: FormatRecord(fmtId=15, fmtCode='d-mmm-yy'),
+        16: FormatRecord(fmtId=16, fmtCode='d-mmm'),
+        17: FormatRecord(fmtId=17, fmtCode='mmm-yy'),
+        18: FormatRecord(fmtId=18, fmtCode='h:mm AM/PM'),
+        19: FormatRecord(fmtId=19, fmtCode='h:mm:ss AM/PM'),
+        20: FormatRecord(fmtId=20, fmtCode='h:mm'),
+        21: FormatRecord(fmtId=21, fmtCode='h:mm:ss'),
+        22: FormatRecord(fmtId=22, fmtCode='m/d/yy h:mm'),
+        37: FormatRecord(fmtId=37, fmtCode='(#,##0_);(#,##0)'),
+        38: FormatRecord(fmtId=38, fmtCode='(#,##0_);[Red](#,##0)'),
+        39: FormatRecord(fmtId=39, fmtCode='(#,##0.00_);(#,##0.00)'),
+        40: FormatRecord(fmtId=40, fmtCode='(#,##0.00_);[Red](#,##0.00)'),
+        41: FormatRecord(fmtId=41, fmtCode='_(* #,##0_);_(* (#,##0);_(* "-"_);_(_)'),
+        42: FormatRecord(fmtId=42, fmtCode='_($* #,##0_);_($* (#,##0);_($* "-"_);_(_)'),
+        43: FormatRecord(fmtId=43, fmtCode='_(* #,##0.00_);_(* (#,##0.00);_(* "-"??_);_(_)'),
+        44: FormatRecord(fmtId=44, fmtCode='_($* #,##0.00_);_($* (#,##0.00);_($* "-"??_);_(_)'),
+        45: FormatRecord(fmtId=45, fmtCode='mm:ss'),
+        46: FormatRecord(fmtId=46, fmtCode='[h]:mm:ss'),
+        47: FormatRecord(fmtId=47, fmtCode='mm:ss.0'),
+        48: FormatRecord(fmtId=48, fmtCode='##0.0E+0'),
+        49: FormatRecord(fmtId=49, fmtCode='@')
+    }
 
     def __init__(self, fp):
         self._fp = fp
@@ -69,9 +72,9 @@ class Styles(object):
         self._fp.seek(0, os.SEEK_SET)
         for rectype, rec in RecordReader(self._fp):
             # TODO
-            if rectype == 47:
+            if rectype == rt.XF:
                 self._xf_record[len(self._xf_record) - 1] = rec
-            elif rectype == 44:
+            elif rectype == rt.FMT:
                 self._format_record[rec.fmtId] = rec
             elif rectype == rt.END_STYLE_SHEET:
                 break
@@ -79,16 +82,16 @@ class Styles(object):
     def get_style(self, idx):
         return None
 
-    def get_number_format(self, idx):
+    def get_format(self, idx):
         if idx is None:
-            return FormatRecord(fmtId=0, fmtCode='General')
+            return self._builtin_formats[0]
         elif idx in self._xf_record:
             numFmtId = self._xf_record[idx].numFmtId
             if numFmtId in self._format_record:
                 return self._format_record[numFmtId]
-            elif numFmtId in self._default_format:
-                return FormatRecord(fmtId=numFmtId, fmtCode=self._default_format[numFmtId]["format"])
-        return FormatRecord(fmtId=0, fmtCode='General')
+            elif numFmtId in self._builtin_formats:
+                return self._builtin_formats[numFmtId]
+        return self._builtin_formats[0]
 
     def get_dtype(self, idx):
         if idx is None:
@@ -96,14 +99,17 @@ class Styles(object):
         elif idx in self._xf_record:
             numFmtId = self._xf_record[idx].numFmtId
             if numFmtId in self._format_record:
-                for char in ["d", "m", "y", "h", "m", "s"]:
-                    if char in self._format_record[numFmtId].fmtCode:
-                        return "datetime"
-                for char in ["0.00", "0,00", "#.##", "#,##"]:
-                    if char in self._format_record[numFmtId].fmtCode:
-                        return "float64"
-            elif numFmtId in self._default_format:
-                return self._default_format[numFmtId]["dtype"]
+                fmtCode = self._format_record[numFmtId].fmtCode
+            elif numFmtId in self._builtin_formats:
+                fmtCode = self._builtin_formats[numFmtId].fmtCode
+            else:
+                return ""
+            for char in ["d", "m", "y", "h", "m", "s"]:
+                if char in fmtCode:
+                    return "datetime"
+            for char in ["0.00", "0,00", "#.##", "#,##"]:
+                if char in fmtCode:
+                    return "float64"
         return ""
 
     def close(self):
