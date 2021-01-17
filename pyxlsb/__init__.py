90fd72cd27428be04ba87b30589aee8a198c1958
William Turner <willtur.will@gmail.com>
2021-01-17 14:06:28 -0500

    Set version to 1.0.9

--- pyxlsb/__init__.py
+++ pyxlsb/__init__.py
@@ -3,7 +3,7 @@ from .reader import BIFF12Reader
 from .workbook import Workbook
 from .worksheet import Worksheet
 
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 def open_workbook(name, debug=False):
   from zipfile import ZipFile
